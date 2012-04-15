eee74ab2beb9de5ece7fe1cbee37f6fdea238f5c
Jens Rantil <jens.rantil@gmail.com>
2012-04-15 23:06:20 +0200

    Adding a TODO about Python version check

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 
+# TODO: Make sure that we are using Python >= 2.7. This needs to be required
+# since we are using the new(er) argparse module we need to enforce this
+# requirement. See supervisor's setup.py for details on how to do this.
+
 setup(
     name='GTDoit',
     version='0.1dev',
