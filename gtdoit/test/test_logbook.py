8e281f294e2f225d7fb4fb428d987e5c905ff8fa
Jens Rantil <jens.rantil@gmail.com>
2012-04-16 01:19:21 +0200

    Pydoc and a comment about testing --help parameter

--- gtdoit/test/test_logbook.py
+++ gtdoit/test/test_logbook.py
@@ -4,6 +4,11 @@ import gtdoit.logbook
 
 
 class TestArgumentParsing(unittest.TestCase):
+    """Tests command line arguments to `logbook`.
+
+    TODO: Test the 'PROG --help' call gives expected output. Don't know how to
+          override sys.exit in best way.
+    """
     def testAtLeastOneEndpointRequired(self):
         exitcode = gtdoit.logbook.main([], exit=False)
         self.assertEqual(exitcode, 2)
