0ed9ee674ea0e77ee586be3df0cf3a1c53e1d64c
Jens Rantil <jens.rantil@gmail.com>
2012-10-09 23:15:59 +0200

    Bumping version to 0.1.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if py_version <= (2, 6):
 
 setup(
     name='rewind',
-    version='0.1.6',
+    version='0.1.7',
     author='Jens Rantil',
     author_email='jens.rantil@gmail.com',
     license='GNU AGPL, version 3',
